bd70418e75cd157acdb849ad2168360dc7f63136
TopFps <TopFps@DESKTOP-UUEU6H0>
2023-01-04 02:26:37 +0300

    Добавьте файлы проекта.

---
+++ Wpf_14/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProductName = "Микроволновка",
+                ProductCost = 3000,
+                ProductImage = "Data/микроволновка.png",
+                ProductType = ProductTypes.Electronics
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Бургер",
+                ProductCost = 200,
+                ProductImage = "Data/бургер.png",
+                ProductType = ProductTypes.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Вентилятор",
+                ProductCost = 7200,
+                ProductImage = "Data/вентилятор.png",
+                ProductType = ProductTypes.Electronics
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Курица",
+                ProductCost = 220,
+                ProductImage = "Data/курица.png",
+                ProductType = ProductTypes.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Носки",
+                ProductCost = 400,
+                ProductImage = "Data/носки.png",
+                ProductType = ProductTypes.Clothes
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Свитер",
+                ProductCost = 5500,
+                ProductImage = "Data/свитер.png",
+                ProductType = ProductTypes.Clothes
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Шапка",
+                ProductCost = 800,
+                ProductImage = "Data/шапка.png",
+                ProductType = ProductTypes.Clothes
+            });
+            lstBox.ItemsSource=products;
+
+        }
+    }
+}
---
+++ Wpf_14/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_14
+{
+     public class Product
+    {
+        public string ProductName { get; set; }
+        public int ProductCost { get; set; }
+        public string ProductImage { get; set; }
+        public ProductTypes ProductType { get; set; }
+    }
+    public enum ProductTypes
+    {
+        Food,
+        Clothes,
+        Electronics
+    }
+
+}
